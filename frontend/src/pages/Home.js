38f8770cf0aeac8ca4e071dfd6e09f058612f96e
Totii676 <128360608+Toti676@users.noreply.github.com>
2025-11-30 03:05:02 +1300

    fixed for deployment on netlify

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
 // import ContactForm from '../components/ContactForm';
